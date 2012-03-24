3a989e933bd2f5d8c50064ee6130486fcdee1e2e
Santiaago <santiago.ariassar@gmail.com>
2012-03-24 22:16:49 +0100

    part31

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,14 +4,10 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    movie.create({:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'});
-    movie.create({:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'});
-    movie.create({:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'});
-    movie.create({:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'});
-    movie.create({:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'});
-    movie.create({:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'});
+    Movie.create!(movie)
   end
-  assert false, "Unimplmemented"
+  #old-assert false, "Unimplmemented"
+  assert movies_table.hashes.size == Movie.all.count
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -20,7 +16,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  assert false, "Unimplmemented"
+  ti = page.all("table#movies tbody tr td[1]").map {|t| t.text}
+  assert ti.index(e1) < ti.index(e2)
+  #old-assert false, "Unimplmemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
